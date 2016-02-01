9a852b38f64729cb6a777857964a9d20534a9366
MLampa <meidenbee@gmail.com>
2016-02-01 11:06:39 -0500

    added pictures to profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
+require_relative '../uploaders/avatar_uploader.rb'
+
 class User < ActiveRecord::Base
+  mount_uploader :avatar, AvatarUploader
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+  validates :username, presence: true, uniqueness: true
   has_many :events
   has_many :event_attendees
   has_many :events, through: :event_attendees
   has_many :votes, through: :event_attendees
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  validates :username, presence: true, uniqueness: true
-
 end
